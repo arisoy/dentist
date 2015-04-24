41df2a1a9a25cdf96cfd894f001e22ddfa68acb6
Mert ARISOY <arisoymert@gmail.com>
2015-04-24 16:08:10 +0300

    Add length and presence validation to Appointment list title

--- spec/features/appointments/create_spec.rb
+++ spec/features/appointments/create_spec.rb
@@ -6,5 +6,49 @@ describe "creating appointments" do
     visit "/appointments"
     click_link "New Appointment"
     expect(page).to have_content("New Appointment")
+    
+    fill_in "Title", with: "My Appointment"
+    fill_in "Description", with: "Dişimi çek la!!!"
+    click_button "Create Appointment"
+    
+    expect(page).to have_content("My Appointment")
+    
   end
+
+  it "displays an error when the appointment has no title" do
+    expect(Appointment.count).to eq(0)
+    
+    visit "/appointments"
+    click_link "New Appointment"
+    expect(page).to have_content("New Appointment")
+    
+    fill_in "Title", with: ""
+    fill_in "Description", with: "Dişimi çek la!!!"
+    click_button "Create Appointment"
+    
+    expect(page).to have_content("error")
+    expect(Appointment.count).to eq(0)
+    
+    visit "/appointments"
+    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+  end
+    
+  it "displays an error when the appointment has a title less than 3 characters" do
+    expect(Appointment.count).to eq(0)
+    
+    visit "/appointments"
+    click_link "New Appointment"
+    expect(page).to have_content("New Appointment")
+    
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: "Dişimi çek la!!!"
+    click_button "Create Appointment"
+    
+    expect(page).to have_content("error")
+    expect(Appointment.count).to eq(0)
+    
+    visit "/appointments"
+    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+  end
+    
 end

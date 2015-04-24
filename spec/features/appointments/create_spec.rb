b423d9cefa9a98558586e740fe36d4c5e88d603d
Mert ARISOY <arisoymert@gmail.com>
2015-04-24 18:09:07 +0300

    Refactor appointments creation in specs

--- spec/features/appointments/create_spec.rb
+++ spec/features/appointments/create_spec.rb
@@ -1,89 +1,70 @@
 require 'spec_helper'
 require 'rails_helper'
 
-describe "creating appointments" do 
-  it "redirects to the appointments index page on success" do
+describe "Creating appointment" do
+  def create_appointment(options={})
+    options[:title] ||= "My appointment"
+    options[:description] ||= "This is my appointment."
+
     visit "/appointments"
     click_link "New Appointment"
     expect(page).to have_content("New Appointment")
-    
-    fill_in "Title", with: "My Appointment"
-    fill_in "Description", with: "Dişimi çek la!!!"
+
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create Appointment"
-    
-    expect(page).to have_content("My Appointment")
-    
   end
 
-  it "displays an error when the appointment has no title" do
+  it "redirects to the appointment index page on success" do
+    create_appointment
+    expect(page).to have_content("My appointment")
+  end
+
+  it "displays an error when the todo list has no title" do
     expect(Appointment.count).to eq(0)
-    
-    visit "/appointments"
-    click_link "New Appointment"
-    expect(page).to have_content("New Appointment")
-    
-    fill_in "Title", with: ""
-    fill_in "Description", with: "Dişimi çek la!!!"
-    click_button "Create Appointment"
-    
+
+    create_appointment title: ""
+
     expect(page).to have_content("error")
     expect(Appointment.count).to eq(0)
-    
+
     visit "/appointments"
-    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+    expect(page).to_not have_content("This is my appointment.")
   end
-    
-  it "displays an error when the appointment has a title less than 3 characters" do
+
+  it "displays an error when the todo list has a title less than 3 characters" do
     expect(Appointment.count).to eq(0)
-    
-    visit "/appointments"
-    click_link "New Appointment"
-    expect(page).to have_content("New Appointment")
-    
-    fill_in "Title", with: "Hi"
-    fill_in "Description", with: "Dişimi çek la!!!"
-    click_button "Create Appointment"
-    
+
+    create_appointment title: "Hi"
+
     expect(page).to have_content("error")
     expect(Appointment.count).to eq(0)
-    
+
     visit "/appointments"
-    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+    expect(page).to_not have_content("This is my appointment.")
   end
-    
+
   it "displays an error when the appointment has no description" do
     expect(Appointment.count).to eq(0)
-    
-    visit "/appointments"
-    click_link "New Appointment"
-    expect(page).to have_content("New Appointment")
-    
-    fill_in "Title", with: "Listing Appointments Title Description New Appointment"
-    fill_in "Description", with: ""
-    click_button "Create Appointment"
-    
+
+    create_appointment title: "Grocery list", description: ""
+
     expect(page).to have_content("error")
     expect(Appointment.count).to eq(0)
-    
+
     visit "/appointments"
-    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+    expect(page).to_not have_content("Grocery list")
   end
-    
-  it "displays an error when the appointment has no description" do
+
+  it "displays an error when the todo list has no description" do
     expect(Appointment.count).to eq(0)
-    
-    visit "/appointments"
-    click_link "New Appointment"
-    expect(page).to have_content("New Appointment")
-    
-    fill_in "Title", with: "Listing Appointments Title Description New Appointment"
-    fill_in "Description", with: "Dis"
-    click_button "Create Appointment"
-    
+
+    create_appointment title: "Grocery list", description: "Food"
+
     expect(page).to have_content("error")
     expect(Appointment.count).to eq(0)
-    
+
     visit "/appointments"
-    expect(page).to have_content("Listing Appointments Title Description New Appointment")
+    expect(page).to_not have_content("Grocery list")
   end
 end
